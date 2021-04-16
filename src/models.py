31b3a49c24a2b733fe355bf74f5309c8d99917d4
socamacho <socamachoruiz@gmail.com>
2021-04-16 23:09:36 +0000

    Finished DB

--- src/models.py
+++ src/models.py
@@ -8,23 +8,65 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True) #El primary_key me ayuda a IDENTIFICAR de manaera equivoca al personaje, planeta, user.
+    username = Column(String(100))
+    firstname = Column(String(100))
+    lastname = Column(String(100))
+    email = Column(String(100))
+    children_post = relationship("Post")
+    children_follower = relationship("Follower")
+    children_comment = relationship("Comment")
+    children_follower2 = relationship("Follower")
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    
+
+
+class Post(Base):
+    __tablename__= 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer,ForeignKey('user.id')) #Cual seria el nombre adecuado?
+    children_media = relationship ("Media")
+    children_comment = relationship("Comment")
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer,primary_key=True)
+    tipo = Column(String(100)) 
+    url = Column(String(100))
+    post_id = Column(Integer,ForeignKey('post.id'))
+    
+    
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(500))
+    author_id = Column(Integer, ForeignKey('user.id')) #DUDA NOMBRE, no seria user id?
+    post_id = Column(Integer,ForeignKey('post.id'))
+
+class Follower(Base): #
+    __tablename__= 'follower' 
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+     
+
+
+
+
+
+
+
+
+
+
+
+
 
     def to_dict(self):
         return {}
